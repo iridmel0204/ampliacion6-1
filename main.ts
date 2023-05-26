387bf2bb82301d9ca49375fe0007616b6d7fa77a
iridmel0204 <132547192+iridmel0204@users.noreply.github.com>
2023-05-26 12:45:01 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
-
+function llamada_manzana () {
+	
+}
+function llamada_inicio () {
+	
+}
+function llamada_final () {
+	
+}
+input.onButtonPressed(Button.AB, function () {
+	
+})
+let juega = false
+basic.forever(function () {
+	
+})
